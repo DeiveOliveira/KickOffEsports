ba092bb135e28cb065cf2107341031d95744ce51
Deive Oliveira <deiveoliveira1@gmail.com>
2023-09-08 14:30:53 -0300

    Add depedencias

---
+++ src/main/java/com/KickOofEsports/KickOffEsports/KickOffEsportsApplication.java
@@ -0,0 +1,13 @@
+package com.KickOofEsports.KickOffEsports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickOffEsportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickOffEsportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/KickOofEsports/KickOffEsports/KickOffEsportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KickOofEsports.KickOffEsports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickOffEsportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
