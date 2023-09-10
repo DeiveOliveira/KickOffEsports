b8e6994f8e4f9e9b7b719d33d400fd28c0ab9bc9
AugustoB1 <guto.brito280802@gmail.com>
2023-09-10 19:30:12 -0300

    Criando o front end do editar e conectando o front end e back end.

--- src/main/java/com/KickOofEsports/KickOffEsports/controllers/EditarController.java
+++ src/main/java/com/KickOofEsports/KickOffEsports/controllers/EditarController.java
@@ -2,6 +2,9 @@ package com.KickOofEsports.KickOffEsports.controllers;
 
 import com.KickOofEsports.KickOffEsports.entities.Usuario;
 import com.KickOofEsports.KickOffEsports.services.EditarService;
+import com.KickOofEsports.KickOffEsports.services.exceptions.EmailDiferentesException;
+import com.KickOofEsports.KickOffEsports.services.exceptions.RecursoNaoEncontradoException;
+import com.KickOofEsports.KickOffEsports.services.exceptions.SenhaDiferenteException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -20,7 +23,7 @@ public class EditarController {
     public ModelAndView editar(@PathVariable String id){
         Optional<Usuario> usuario = service.procurarPorId(id);
         ModelAndView editar = new ModelAndView();
-        editar.addObject("usuario", usuario);
+        usuario.ifPresent(u -> editar.addObject("usuario", u));
         editar.setViewName("Cadastro");
         System.out.println("pesquisou com sucesso o usuario do id: " + id);
         return editar;
@@ -28,8 +31,19 @@ public class EditarController {
 
     @PutMapping(value = "editarUsuario/{id}")
     public ResponseEntity<?> editar(@PathVariable String id, @RequestBody Usuario usuario){
-        usuario = service.atualizar(id, usuario);
-        return ResponseEntity.ok().body(usuario);
+        try{
+            usuario = service.atualizar(id, usuario);
+            return ResponseEntity.ok().body(usuario);
+        }
+        catch (SenhaDiferenteException e){
+            return ResponseEntity.badRequest().body("Senha diferente!");
+        }
+        catch (EmailDiferentesException e){
+            return ResponseEntity.badRequest().body("Email não é alteravel");
+        }
+        catch (RecursoNaoEncontradoException e){
+            return ResponseEntity.badRequest().body("Usuario não encontrado.");
+        }
     }
 
 }
--- src/main/java/com/KickOofEsports/KickOffEsports/services/exceptions/CpfDiferentesExceptions.java
+++ src/main/java/com/KickOofEsports/KickOffEsports/services/exceptions/CpfDiferentesExceptions.java
@@ -3,7 +3,5 @@ package com.KickOofEsports.KickOffEsports.services.exceptions;
 public class CpfDiferentesExceptions extends RuntimeException{
     private static final long serialVersionUID = 1L;
 
-    public CpfDiferentesExceptions() {
-        super("Cpf não é alteravel");
-    }
+    public CpfDiferentesExceptions() { super("Cpf não é alteravel"); }
 }
